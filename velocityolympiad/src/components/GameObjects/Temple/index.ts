a115a6cc4b0231be4db64bdff9dd71d364102894
Mahussi DATONGNON <84915773+mahussidatongnon@users.noreply.github.com>
2024-03-10 16:46:26 +0100

    add wall to template

--- velocityolympiad/src/components/GameObjects/Temple/index.ts
+++ velocityolympiad/src/components/GameObjects/Temple/index.ts
@@ -1,19 +1,25 @@
-import {Scene, Vector3, Color3, StandardMaterial, ArcRotateCamera, MeshBuilder, HemisphericLight,
-    FreeCamera, TransformNode, Camera
+import {
+    Vector3, Color3, StandardMaterial, ArcRotateCamera, MeshBuilder, HemisphericLight,
+    TransformNode, Camera, Mesh
 } from "@babylonjs/core";
 import {OurScene} from "../../../BabylonCodes/scenes";
 import {Stairs} from "../Stairs";
 
 export class Temple {
-    camera: Camera;
+    camera: Camera | undefined;
     ourScene: OurScene;
-    _pillarMaterial: StandardMaterial;
+    _pillarMaterial: StandardMaterial | undefined;
     boxHeight: number; // hauteur du temple
     boxWidth: number; // largeur du temple
     boxDepth: number; // profondeur du temple
     pillarHeight: number; // hauteur des piliers
     pillarDiameter: number = 0.25; // diamètre de la boîte
     private readonly templeGroup: TransformNode;
+    private _doorPositions: { x: number[]; y: number[]; z: number[] };
+    private _doorIndexes: number[];
+    private _numberOfPillars: number;
+    private _spacingPillars: number;
+    private _wallMaterial: StandardMaterial;
 
     constructor(ourScene: OurScene, boxHeight: number = 1, boxWidth: number = 10, boxDepth: number = 5) {
         this.ourScene = ourScene;
@@ -22,7 +28,33 @@ export class Temple {
         this.boxDepth = boxDepth;
         this.pillarHeight = 6; // hauteur des piliers
         this.templeGroup = new TransformNode('templeGroup', this.ourScene.scene);
+        this._doorIndexes = [2, 3];
+        this._doorPositions = {x: [], y: [], z: []};
+        this._numberOfPillars = this.boxWidth; // nombre de piliers sur un côté
+        this._spacingPillars = 1; // espacement entre les piliers
+        this._wallMaterial =  new StandardMaterial("wallMaterial", this.ourScene.scene)
+        this._wallMaterial.diffuseColor = new Color3(0.8, 0.8, 0.7); // Exemple de couleur
         this.setup();
+    }
+
+    set doorIndexes(value: number[]) {
+        this._doorIndexes = value;
+    }
+
+    get doorIndexes(): number[] {
+        return this._doorIndexes;
+    }
+
+    set numberOfPillars(value: number) {
+        this._numberOfPillars = value;
+    }
+
+    get numberOfPillars(): number {
+        return this._numberOfPillars;
+    }
+
+    set spacingPillars(value: number) {
+        this._spacingPillars = value;
     }
 
     set position(position: Vector3) {
@@ -31,6 +63,14 @@ export class Temple {
 
     get position(): Vector3 {
         return this.templeGroup.position;
+    }
+
+    set wallMaterial(value: StandardMaterial) {
+        this._wallMaterial = value;
+    }
+
+    get wallMaterial(): StandardMaterial {
+        return this._wallMaterial;
     }
 
     setCamera(camera: Camera) {
@@ -74,8 +114,8 @@ export class Temple {
         this._pillarMaterial = new StandardMaterial('pillarMaterial', this.ourScene.scene);
         this._pillarMaterial.diffuseColor = new Color3(1, 0.9, 0.8); // couleur de la pierre
 
-        let numberOfPillars = this.boxWidth; // nombre de piliers sur un côté
-        let spacing = 1; // espacement entre les piliers
+        let numberOfPillars = this._numberOfPillars // nombre de piliers sur un côté
+        let spacing = this._spacingPillars; // espacement entre les piliers
 
         // Côté avant du temple
         // Les côtés du temple
@@ -88,23 +128,18 @@ export class Temple {
             pillar.position.z = this.boxDepth/2; // positionner devant
             pillar.position.y = this.pillarHeight / 2 - this.boxHeight/2; // positionner au-dessus du sol
             pillar.parent = this.templeGroup;
-            if(i == 0 || i == numberOfPillars) wallPillars.push(pillar);
         }
-        this.createWall(wallPillars[0], wallPillars[1]);
 
         // Côté arrière du temple
-        wallPillars.shift(); // retirer le premier élément
         for (let i = 0; i < numberOfPillars; i++) {
             let pillar = this.createPillar(this.ourScene.scene, 'pillar_back' + i);
             pillar.position.x = (i - numberOfPillars / 2) * spacing;
             pillar.position.z = -this.boxDepth/2; // positionner derrière
             pillar.position.y = this.pillarHeight / 2 - this.boxHeight/2; // positionner au-dessus du sol
             pillar.parent = this.templeGroup;
-            if(i+1 == numberOfPillars) wallPillars.push(pillar);
         }
-        this.createWall(wallPillars[0], wallPillars[1], Math.PI/2, 0, 2*this.pillarDiameter);
+        // this.createWall(wallPillars[0], wallPillars[1], Math.PI/2, 0, 2*this.pillarDiameter);
 
-        wallPillars.shift(); // retirer le premier élément
         let sidePillars = this.boxDepth;
         for (let i = 0; i < sidePillars; i++) {
             let pillarRight = this.createPillar(this.ourScene.scene, 'pillar_right' + i);
@@ -113,19 +148,31 @@ export class Temple {
             pillarRight.position.y = this.pillarHeight / 2 - this.boxHeight/2; // positionner au-dessus du sol
             pillarRight.parent = this.templeGroup;
 
-            if (i == 2 || i == 3) continue; // laisser un espace pour les portes
+            if (this._doorIndexes.includes(i)) {
+                // laisser un espace pour les portes
+                this._doorPositions.x.push(pillarRight.position.x);
+                this._doorPositions.y.push(pillarRight.position.y);
+                this._doorPositions.z.push(pillarRight.position.z);
+                continue;
+            }
             let pillarLeft = this.createPillar(this.ourScene.scene, 'pillar_left' + i);
             pillarLeft.position.x = -(numberOfPillars / 2) * spacing;
             pillarLeft.position.z = (i - sidePillars / 2) * spacing;
             pillarLeft.position.y = this.pillarHeight / 2 - this.boxHeight/2; // positionner au-dessus du sol
             pillarLeft.parent = this.templeGroup;
-
         }
 
         // Ajouter un toit incliné au temple
         this.buildRoof();
 
         // Créer des escaliers devant le temple
+        this.createStairs();
+
+        this.createTempleWalls(templeBox);
+
+    }
+
+    createStairs() {
         const stairStep = 5; // Nombre de marches
         const stepHeight = 0.2; // Hauteur de chaque marche
         const stepWidth = this.boxDepth; // Largeur de chaque marche (pourrait correspondre à la largeur de l'entrée du temple)
@@ -136,8 +183,6 @@ export class Temple {
         const stairsPositionZ = - this.boxDepth - stairStep/2*stepDepth ;
         const stairsHeightAdjustment = - this.boxHeight/2;
         stairs.position = new Vector3(stairsPositionZ, stairsHeightAdjustment, 0);
-
-
     }
 
     createPillar(scene, pillarName=undefined) {
@@ -147,8 +192,7 @@ export class Temple {
             height: this.pillarHeight, // hauteur du pilier
             tessellation: 24 // nombre de côtés du cylindre, pour le rendre rond
         }, scene);
-        pillar.material = this._pillarMaterial;
-
+        if (this._pillarMaterial != undefined) pillar.material = this._pillarMaterial;
         return pillar;
     }
 
@@ -191,6 +235,86 @@ export class Temple {
         roofRight.material = roofMaterial;
         fronton.material = roofMaterial;
     }
+
+    createTempleWalls(templeBox: Mesh) {
+        // Créer les murs du temple
+        // Calculer les coins supérieur droit et inférieur gauche du temple du mur arrière
+        let upperRightCorner = new Vector3(templeBox.position.x + this.boxWidth/2,
+            templeBox.position.y + this.pillarHeight - this.boxHeight/2, templeBox.position.z + this.boxDepth/2);
+        let lowerLeftCorner = new Vector3(templeBox.position.x - this.boxWidth/2,
+            templeBox.position.y - this.boxHeight/2,templeBox.position.z + this.boxDepth/2);
+        this.createWallV2("wall_back", upperRightCorner, lowerLeftCorner);
+
+        // Calculer les coins supérieur droit et inférieur gauche du temple du mur de droite
+        upperRightCorner = new Vector3(templeBox.position.x + this.boxDepth/2,
+            templeBox.position.y + this.pillarHeight - this.boxHeight/2, templeBox.position.z);
+        lowerLeftCorner = new Vector3(templeBox.position.x - this.boxDepth/2,
+            templeBox.position.y - this.boxHeight/2,templeBox.position.z);
+        let wallRight = this.createWallV2("wall_right", upperRightCorner, lowerLeftCorner);
+        wallRight.position.x += this.boxWidth/2;
+        wallRight.rotation.y = Math.PI/2;
+
+        // Calculer les coins supérieur droit et inférieur gauche du temple du mur d'avant
+        upperRightCorner = new Vector3(templeBox.position.x + this.boxWidth/2,
+            templeBox.position.y + this.pillarHeight - this.boxHeight/2, templeBox.position.z - this.boxDepth/2);
+        lowerLeftCorner = new Vector3(templeBox.position.x - this.boxWidth/2,
+            templeBox.position.y - this.boxHeight/2,templeBox.position.z - this.boxDepth/2);
+        this.createWallV2("wall_front", upperRightCorner, lowerLeftCorner);
+
+        // Calculer les coins supérieur droit et inférieur gauche du temple du mur de gauche
+        // mur avant la porte
+        let doorMinZ = Math.min(...this._doorPositions.z);
+        let doorMaxZ = Math.max(...this._doorPositions.z);
+        let wallLeftBeforeDoorDistance = Math.abs(doorMinZ - this._spacingPillars + this.boxDepth/2)
+        upperRightCorner = new Vector3(templeBox.position.x + wallLeftBeforeDoorDistance,
+            templeBox.position.y + this.pillarHeight - this.boxHeight/2, templeBox.position.z);
+        lowerLeftCorner = new Vector3(templeBox.position.x,
+            templeBox.position.y - this.boxHeight/2,templeBox.position.z);
+        let wallLeftBeforeDoor = this.createWallV2("wall_left_before_door", upperRightCorner, lowerLeftCorner);
+        wallLeftBeforeDoor.position.x = templeBox.position.x - this.boxWidth/2;
+        wallLeftBeforeDoor.rotation.y = Math.PI/2;
+        wallLeftBeforeDoor.position.z = doorMinZ - this._spacingPillars - wallLeftBeforeDoorDistance/2;
+
+        // mur après la porte
+        let wallLeftAfterDoorDistance = Math.abs(doorMaxZ + this._spacingPillars - this.boxDepth/2);
+        upperRightCorner = new Vector3(templeBox.position.x + wallLeftAfterDoorDistance,
+            templeBox.position.y + this.pillarHeight - this.boxHeight/2, templeBox.position.z);
+        lowerLeftCorner = new Vector3(templeBox.position.x,
+            templeBox.position.y - this.boxHeight/2,templeBox.position.z);
+        let wallLeftAfterDoor = this.createWallV2("wall_left_after_door", upperRightCorner, lowerLeftCorner);
+        wallLeftAfterDoor.position.x = templeBox.position.x - this.boxWidth/2;
+        wallLeftAfterDoor.rotation.y = Math.PI/2;
+        wallLeftAfterDoor.position.z = doorMaxZ + this._spacingPillars + wallLeftAfterDoorDistance/2;
+    }
+
+    createWallV2(name, upperRightCorner: Vector3, lowerLeftCorner: Vector3, thickness: number = 0.02): Mesh {
+        // Calculer la largeur et la hauteur du mur
+        const width = Math.abs(upperRightCorner.x - lowerLeftCorner.x);
+        const height = Math.abs(upperRightCorner.y - lowerLeftCorner.y);
+        const depth = thickness;
+
+        // Calculer la position centrale du mur
+        const centerX = (upperRightCorner.x + lowerLeftCorner.x) / 2;
+        const centerY = (upperRightCorner.y + lowerLeftCorner.y) / 2;
+        const centerZ = (upperRightCorner.z + lowerLeftCorner.z) / 2; // Supposant que z est la même pour les deux coins
+
+        // Créer une mesh box pour représenter le mur
+        const wall = MeshBuilder.CreateBox(name, {
+            width: width,
+            height: height,
+            depth: depth
+        }, this.ourScene.scene);
+
+        // Définir la position du mur
+        wall.position.x = centerX;
+        wall.position.y = centerY;
+        wall.position.z = centerZ;
+        wall.material = this._wallMaterial;
+        wall.parent = this.templeGroup;
+
+        return wall;
+    }
+
 
     createWall(pillar1: TransformNode, pillar2: TransformNode, rotationY: number = 0, positionXMinus = 0, positionXManus = 0) {
         // Dimensions et position du mur
