5145e543f5ba15ed75fcd2da9ae51790585d0955
samsoucoupe <samybendhiab@gmail.com>
2024-05-11 16:59:34 +0200

    cardlist don't use player

--- src/gameObjects/Card/ICard.ts
+++ src/gameObjects/Card/ICard.ts
@@ -1,11 +1,12 @@
 import {RareteCard} from "./RareteCard.ts";
 import {Character} from "../../character/interfaces/Character.ts";
 import {Player} from "../../character/players";
+import {Scene, Vector3} from "@babylonjs/core";
 
 
 
 export interface ICard {
-    firstSpell(player:Player): void;
+    firstSpell(scene : Scene, position : Vector3): void;
     secondSpell(): void;
     name: string;
     description: string;
