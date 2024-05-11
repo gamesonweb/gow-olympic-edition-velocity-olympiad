6d3b4a6fec90349886f2defa9484920153180cfa
samsoucoupe <samybendhiab@gmail.com>
2024-05-11 16:02:46 +0200

    add fireball assets

--- src/gameObjects/Card/armes/FlammeCard.ts
+++ src/gameObjects/Card/armes/FlammeCard.ts
@@ -4,21 +4,88 @@ import {FirstPersonPlayer} from "../../../../BabylonCodes/Character/players/firs
 import {
     Color3, Color4,
     MeshBuilder,
-    ParticleSystem,
+    ParticleSystem, Path3D,
     PhysicsAggregate,
     PhysicsShapeType,
     StandardMaterial, Texture, Vector3
 } from "@babylonjs/core";
-import {Character} from "../../../character/interfaces/Character.ts";
+
+import {Player} from "../../../character/players";
 
 
-export class FlammeCard implements ICard{
-    firstSpell(player : Character): void {
+export class FlammeCard implements ICard {
 
-        //     on va crée une boule de feu avec une trainer de feu qui part du joueur
+    firstSpell(player: Player): void {
+    let color1 = Color3.FromInts(249, 115, 0);
+    let color2 = Color3.FromInts(222, 93, 54);
+    let texture = "https://raw.githubusercontent.com/oriongunning/t5c/main/public/textures/particle_01.png";
+    let textureParticule = new Texture(texture);
+    let scene = player._scene;
 
-        console.log('First spell');
-    }
+    let camera = scene.activeCamera;
+    if (!camera) return; // Return if there's no active camera
+
+    let start = player.position.clone();
+    start.y += 1;
+
+    // Calculate end position based on camera direction
+    let end = camera.getTarget().subtract(camera.position).normalize().scaleInPlace(100).add(camera.position);
+
+    // calculate angle
+    var angle = Math.atan2(start.z - end.z, start.x - end.x);
+
+    // create material
+    var material = new StandardMaterial("player_spell");
+    material.diffuseColor = color1;
+    material.alpha = 0.1;
+
+    // create mesh
+    var projectile = MeshBuilder.CreateSphere("Projectile", {segments: 4, diameter: 0.4}, scene);
+    projectile.material = material;
+    projectile.position = start.clone();
+    projectile.rotation.y = Math.PI / 2 - angle;
+
+    //particule
+    var particleSystem = new ParticleSystem("particles", 2000, scene);
+    particleSystem.particleTexture = textureParticule;
+    particleSystem.emitter = projectile;
+    particleSystem.minEmitBox = new Vector3(0, 0, 0);
+    particleSystem.maxEmitBox = new Vector3(0, 0, 0);
+    particleSystem.color1 = color1;
+    particleSystem.color2 = color2;
+    particleSystem.colorDead = new Color4(0, 0, 0, 0.0);
+    particleSystem.minSize = 0.1;
+    particleSystem.maxSize = 0.5;
+    particleSystem.minLifeTime = 0.3;
+    particleSystem.maxLifeTime = 1.5;
+    particleSystem.emitRate = 1500;
+    particleSystem.blendMode = ParticleSystem.BLENDMODE_ONEONE;
+    particleSystem.gravity = new Vector3(0, -9.81, 0);
+    particleSystem.direction1 = new Vector3(-7, 8, 3);
+    particleSystem.direction2 = new Vector3(7, 8, -3);
+    particleSystem.minAngularSpeed = 0;
+    particleSystem.maxAngularSpeed = Math.PI;
+    particleSystem.minEmitPower = 1;
+    particleSystem.maxEmitPower = 3;
+    particleSystem.updateSpeed = 0.005;
+    particleSystem.start();
+
+    // move
+    var endVector = projectile.calcMovePOV(0, 0, 100).addInPlace(projectile.position);
+    var points = [start, endVector];
+    var path = new Path3D(points);
+    var i = 0;
+    var loop = scene.onBeforeRenderObservable.add(() => {
+        if (i < 1) {
+            projectile.position = path.getPointAt(i);
+            i += 0.001;
+        } else {
+            projectile.dispose();
+            scene.onBeforeRenderObservable.remove(loop);
+        }
+    });
+}
+
 
     secondSpell(): void {
         console.log('Second spell');
@@ -27,7 +94,7 @@ export class FlammeCard implements ICard{
     name: string = 'Flamme';
     description: string = 'Une carte de flamme';
     meshname: string;
-    rarete: RareteCard ;
+    rarete: RareteCard;
 
     constructor(rarete: RareteCard) {
         this.rarete = rarete;
@@ -50,8 +117,6 @@ export class FlammeCard implements ICard{
     setup() {
         // Setup the card
     }
-
-
 
 
 }
