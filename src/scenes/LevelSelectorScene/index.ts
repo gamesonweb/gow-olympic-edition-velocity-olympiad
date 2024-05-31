12a007d8ced1eaf19c229d125e587f377b836e64
Yaplupile <yassine.mahjoub2001@gmail.com>
2024-05-31 18:14:24 +0200

    push bordel tuto

--- src/scenes/LevelSelectorScene/index.ts
+++ src/scenes/LevelSelectorScene/index.ts
@@ -17,11 +17,15 @@ import {OlympiadScene} from "../OlympiadScene";
 import {FirstLevelEnemyManager} from "./enemyManager";
 import {Player, PlayerState} from "../../character/players";
 import {TempleV2} from "../../gameObjects/TempleV2";
-import {EnemyManager} from "../EnemyManager.ts";
 import { Sign } from "../../gameObjects/Sign/index.ts";
+import { JumpCard } from "../../gameObjects/Card/armes/JumpCard";
+import { FlammeCard } from "../../gameObjects/Card/armes/FlammeCard";
+import { RareteCard } from "../../gameObjects/Card/RareteCard";
+import { CardSocle } from "../../gameObjects/Card/CardSocle";
+
 
 export class LevelSelectorScene extends OlympiadScene {
-    protected readonly enemyManager: EnemyManager;
+    protected readonly enemyManager: FirstLevelEnemyManager
     // noinspection JSUnusedGlobalSymbols
     private _meshes: Mesh[] = [];
     private _materials: Material[] = [];
@@ -34,6 +38,14 @@ export class LevelSelectorScene extends OlympiadScene {
         this.addComponent(this.enemyManager); // Ainsi, le manager sera détruit avec la scène
 
         this.player = new Player(playerState, this);
+    }
+
+    public onPauseState() {
+        return
+    }
+
+    public onResumeState() {
+        return
     }
 
     public async init(): Promise<void> {
@@ -61,7 +73,6 @@ export class LevelSelectorScene extends OlympiadScene {
 
             for (let child of childrens) {
                 const mesh = child as Mesh;
-                mesh.renderingGroupId = 2;
                 const body = new PhysicsBody(mesh, PhysicsMotionType.STATIC, false, this);
                 body.shape = new PhysicsShapeMesh(mesh, this)
             }
@@ -87,12 +98,52 @@ export class LevelSelectorScene extends OlympiadScene {
         //Signs 
 
         let signs = [
-            {text : "Appuiez sur ZQSD pour vous deplacez", position : new Vector3(0, 8.5, -78)},
+            {text : "Appuiez sur ZQSD pour vous Deplacer", position : new Vector3(0, 8.5, -78)},
+            {text : "Appuiez sur \"Espace\" pour Sauter", position : new Vector3(-1.7, 8.5, -35)},
+            {text : "Appuiez sur \"Maj Gauche\" pour vous propulser dans les airs", position : new Vector3(-92, 22, 91)},
+            {text : "Ceci est un Pouvoir de Vitesse \n Appuiez sur \"E\" Afin d'accomplir un Double Saut , Vous pouvez l'uiliser autant de fois que ça durabilité vous le permet", position : new Vector3(-95, 29, 188)},
+            {text : "Les Pouvoirs \n possèdent aussi une utilisation Consommant toute leur durabilité d'un coup appuiez sur \"A\"\nAvec ce Pouvoir de vitesse pour augementer votre vitesse de Déplacement pendant un cours Laps de temps", position : new Vector3(-43, 58, 196)},
+            {text : "Voici La Torche, celle-ci permet d'envoyer des boules de feu avec son \"A\" et d'effectuer une propulstion capable de battre des ennemis avec \"E\"" , position : new Vector3(78, 88, 273)},
+            {text : "Une fois être rentré dans le temple, votre but va être de raviver la Flamme dans le temple de l'olympe" , position : new Vector3(78, 88, 273)},
         ]
+
 
         signs.forEach(element => {
             let sign = new Sign(element.text, element.position, this);
             this.addComponent(sign);
+        });
+
+        //Cards 
+
+        let cards = [
+            {card: new JumpCard(RareteCard.RARE), position: new Vector3(-95, 29, 188)},
+            {card: new JumpCard(RareteCard.RARE), position: new Vector3(-43, 58, 196)},
+            {card: new JumpCard(RareteCard.RARE), position: new Vector3(-43, 58, 196)},
+            {card: new FlammeCard(RareteCard.EPIC), position: new Vector3(75, 88, 270)},
+            {card: new FlammeCard(RareteCard.EPIC), position: new Vector3(78, 88, 273)},
+            {card: new FlammeCard(RareteCard.EPIC), position: new Vector3(81, 88, 276)},
+
+        ]
+
+
+        cards.forEach(cardAndPosition => {
+            let cardSocle = new CardSocle(this, cardAndPosition.card, cardAndPosition.position);
+            this.addComponent(cardSocle);
+            this.addGameObject(cardSocle);
+            if (cardSocle.card instanceof FlammeCard) {
+                this.addComponent(cardSocle.card.projectile);
+                this.addGameObject(cardSocle.card.projectile);
+            }
+        })        
+        
+        let ennemis = [
+            new Vector3(121, 87, 348),
+            new Vector3(121, 87, 281),
+            new Vector3(176, 87, 249),
+        ]
+
+        ennemis.forEach(position => {
+            this.enemyManager.addDistanceEnemy(position);
         });
 
 
