03b375f073e6bed6eb24d944c2e3ec3cf4453880
Mahussi DATONGNON <84915773+mahussidatongnon@users.noreply.github.com>
2024-06-01 15:37:18 +0200

    custom asset loading, init scene when player ready and assets loaded

--- src/gameObjects/Card/armes/FlammeCard.ts
+++ src/gameObjects/Card/armes/FlammeCard.ts
@@ -5,6 +5,7 @@ import {Scene, Vector3} from "@babylonjs/core";
 import {FlammeCardProjectile} from "./FlammeCardProjectile.ts";
 import {Player} from "../../../character/players";
 import {OlympiadScene} from "../../../scenes/OlympiadScene.ts";
+import {PublicAssetsModel} from "../../../publicAssets/PublicAssetsModel.ts";
 
 export class FlammeCard implements ICard {
 
@@ -20,22 +21,22 @@ export class FlammeCard implements ICard {
         this.rarete = rarete;
         switch (rarete) {
             case RareteCard.COMMON:
-                this.meshname = "TorchCardGray.glb";
+                this.meshname = PublicAssetsModel.TorchCardGray;
                 this.damage = 10;
                 this.durabilite = 1;
                 break;
             case RareteCard.RARE:
-                this.meshname = "TorchCardBlue.glb"
+                this.meshname = PublicAssetsModel.TorchCardBlue;
                 this.damage = 20;
                 this.durabilite = 2;
                 break;
             case RareteCard.EPIC:
-                this.meshname = "TorchCardPurple.glb";
+                this.meshname = PublicAssetsModel.TorchCardPurple;
                 this.damage = 50;
                 this.durabilite = 3;
                 break;
             case RareteCard.LEGENDARY:
-                this.meshname = "TorchCardGold.glb";
+                this.meshname = PublicAssetsModel.TorchCardGold;
                 this.damage = 100;
                 this.durabilite = 5;
                 break;
--- src/scenes/SceneManager.ts
+++ src/scenes/SceneManager.ts
@@ -6,6 +6,7 @@
 
 import {Engine} from "@babylonjs/core";
 import {PlayerState} from "../character/players";
+import {OlympiadScene} from "./OlympiadScene.ts";
 
 export class SceneManager {
 
@@ -21,12 +22,37 @@ export class SceneManager {
         this.playerState = new PlayerState();
     }
 
+    get assetLoaded(): boolean {
+        return (this.engine.scenes[0] as OlympiadScene).isSceneReady;
+    }
+
+    get playerReady(): boolean {
+        if(!this.assetLoaded) return false;
+        return !this.engine.scenes[0].activeCamera;
+    }
+
     public renderScene() {
         if (!this.engine.scenes[0]) throw new Error("No active scene set.");
 
         this.engine.runRenderLoop(() => {
             // Render the scene if not stopped
-            this.engine.scenes[0].render();
+            let scene = this.engine.scenes[0] as OlympiadScene;
+            if (!this.assetLoaded) {
+                let loopRenderSceneWhenReady = setInterval(() => {
+                    if (this.assetLoaded) {
+                        if (this.playerReady) {
+                            clearInterval(loopRenderSceneWhenReady);
+                            this.engine.scenes[0].render();
+                        } else {
+                            console.log("Scene not ready yet: Player loading...")
+                        }
+                    } else {
+                        console.log("Scene not ready yet: Asset loading...");
+                    }
+                }, 1000);
+            } else {
+                scene.render();
+            }
         });
     }
 }
